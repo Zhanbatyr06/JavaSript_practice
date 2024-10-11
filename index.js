df7b89a138a273ca96e394dc4f7cadd4986e9eb1
Zhanbatyr06 <157192953+Zhanbatyr06@users.noreply.github.com>
2024-10-11 19:32:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-function multiplyBy()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("res").innerHTML = num1 * num2;
-}
-
-function divideBy() 
-{ 
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-document.getElementById("res").innerHTML = num1 / num2;
-}
-
-
+function cToF(celsius) {
+        var cTemp = celsius;
+      
+        var cToFahr = cTemp * 9 / 5 + 32;
+      
+        var message = cTemp + '\xB0C is ' + cToFahr + ' \xB0F.';
+      
+        console.log(message);
+      }
+      
+      function fToC(fahrenheit) {
+        var fTemp = fahrenheit;
+      
+        var fToCel = (fTemp - 32) * 5 / 9;
+      
+        var message = fTemp + '\xB0F is ' + fToCel + '\xB0C.';
+      
+        console.log(message);
+      }
+      
+      cToF(60);
+      
+      fToC(45); 
+      
